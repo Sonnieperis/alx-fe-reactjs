35f06a0ab5a7bc3ffc50be4ebb6697fe35e5b6d1
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-07-23 19:55:08 +0300

    full company site with routing

---
+++ alx-react-app-props/my-company/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: 'white',
+      padding: '10px',
+      textAlign: 'center',
+      marginTop: '20px'
+    }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking, React, and photography"
-      />
-      <Footer />
+      <h1>React Counter App</h1>
+      <Counter />
     </div>
   );
 }
+
 
 export default App;
