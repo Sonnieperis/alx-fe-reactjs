3a042b3bd681b24e944bc3a48a02335aaf14de06
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-07-27 20:05:02 +0300

    Initialize recipe-sharing-app with Zustand state

---
+++ src/store/recipeStore.js
@@ -0,0 +1,14 @@
+// src/store/recipeStore.js
+
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export { useRecipeStore };
