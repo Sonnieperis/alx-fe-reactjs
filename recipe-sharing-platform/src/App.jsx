5a4f123c5154268f9cc06ac8b00fb92d1e3a60b1
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-08-15 18:51:14 +0300

    recipe-sharing-platform (Vite + React + Tailwind)

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind is working ✅
+      </h1>
+    </div>
+  );
+}
