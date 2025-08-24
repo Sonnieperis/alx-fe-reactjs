d984adbaef9e71666acad4e64839665d60d6f358
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-08-24 21:37:47 +0300

    inittial commit

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings() {
+  return (
+    <div>
+      <h3>Profile Settings</h3>
+      <p>Preferences and account settings would go here.</p>
+    </div>
+  );
+}
