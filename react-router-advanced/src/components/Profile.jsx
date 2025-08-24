52634c4abc7d21dacdc08a6ea59e7677af27ec78
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-08-24 21:44:30 +0300

    add profile.jsx

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function ProfileDetails() {
+  return <h3>Profile Details Section</h3>;
+}
+
+function ProfileSettings() {
+  return <h3>Profile Settings Section</h3>;
+}
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Nested Routes inside Profile */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Profile;
